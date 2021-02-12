fb14374aa18e5fecfe7fae5877dd87780defd24b
tatey12 <tatebaldwin788@gmail.com>
2021-02-12 06:11:02 -0500

    Update vombot.js

--- vombot.js
+++ vombot.js
@@ -1,21 +1,5 @@
 var discord_interface = require('./lib/discord_interface.js');
 var markov = require('./lib/markov.js');
-var ws_server = require('./lib/ws_server.js');
-
-
-
-
-/*
-	trying to replace twitch alerts with new follower alerts
-curl.get('https://api.twitch.tv/kraken/channels/puke7/follows', {
-	HTTPHEADER: 'Accept: application/vnd.twitchtv.v3+json'
-}, function(err) {
-	//console.info(this);
-});
-*/
-
-
-/*
- * initialize services
- */
+// I am not using the microservice
 discord_interface.initialize();
+markov.initialize('markov_log.txt');
